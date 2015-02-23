rogora/hangish

commit 48c23efc1ff3b40bc75b536b4ef00bf3abf15e9f

Fix #5: Fix parsing of [ and ] in messages

utils.cpp
@@ -122,15 +122,21 @@ int Utils::skipFields(QString input, int startPos)
     int res = startPos;
     int last = startPos;
     int openbracket = 0, closebracket = 0;
+    int quotes = 0;
 
     for (; ;) {
         res = input.indexOf(",", res);
         if (res==-1) {
             return input.lastIndexOf("]");
         }
+        int escaped = -1;
         for (int j=last; j<res; j++) {
-            openbracket += input.at(j)=='[';
-            closebracket += input.at(j)==']';
+            if (input.at(j) == '\\') escaped = j;
+            if (escaped != j-1) quotes += input.at(j) == '"'; // If a quote is escaped it is in a string so ignore it.
+            if (quotes % 2 == 0) { // We only care for brackets which are not in quotes (e.g. strings)
+                openbracket += input.at(j)=='[';
+                closebracket += input.at(j)==']';
+            }
         }
         if (openbracket == closebracket)
         {
@@ -201,7 +207,7 @@ EventValueSegment Utils::parseEventValueSegment(QString segment)
     } else if (res.type == 1) { // NEW LINE
         res.value = "";
     } else if (res.type == 2) { // LINK
-        auto matchIt = STRING.globalMatch(segment);
+        QRegularExpressionMatchIterator matchIt = STRING.globalMatch(segment);
         int matches = 0;
         while (matchIt.hasNext()) {
             QString match = matchIt.next().captured();

commit 0d1e7f3d646cd12f52bbe127f62d9fdd61fac540

Fix errors introduced by 48c23ef

client.cpp
@@ -276,9 +276,11 @@ Conversation Client::parseConversationDetails(QString conversation, Conversation
 {
     int start = 1;
     for (;;) {
-        QString tmp_event = Utils::getNextAtomicField(conversation, start);
+        QString tmp_event = Utils::getNextAtomicField(conversation, start, false);
         if (tmp_event.size()<10) break;
-        res.events.append(Utils::parseEvent(tmp_event));
+        Event e = Utils::parseEvent(tmp_event);
+        if (e.value.valid)
+            res.events.append(e);
     }
     return res;
 }

utils.cpp
@@ -49,9 +49,9 @@ QString Utils::getTextAtomicField(QString conv, int &start)
 }
 
 
-QString Utils::getNextAtomicField(QString conv, int &start)
+QString Utils::getNextAtomicField(QString conv, int &start, bool parseBracketsInString)
 {
-    int stop = skipFields(conv, start);
+    int stop = skipFields(conv, start, parseBracketsInString);
     if (stop-start==0) {
         return "";
     }
@@ -115,7 +115,7 @@ int Utils::skipTextFields(QString input, int startPos)
     }
 }
 
-int Utils::skipFields(QString input, int startPos)
+int Utils::skipFields(QString input, int startPos, bool parseBracketsInString)
 {
     //Check for empty field
 
@@ -133,7 +133,7 @@ int Utils::skipFields(QString input, int startPos)
         for (int j=last; j<res; j++) {
             if (input.at(j) == '\\') escaped = j;
             if (escaped != j-1) quotes += input.at(j) == '"'; // If a quote is escaped it is in a string so ignore it.
-            if (quotes % 2 == 0) { // We only care for brackets which are not in quotes (e.g. strings)
+            if (parseBracketsInString || quotes % 2 == 0) { // We only care for brackets which are not in quotes (e.g. strings)
                 openbracket += input.at(j)=='[';
                 closebracket += input.at(j)==']';
             }
@@ -198,7 +198,7 @@ EventValueSegment Utils::parseEventValueSegment(QString segment)
 {
     EventValueSegment res;
     int start = 1;
-    res.type = getNextAtomicField(segment, start).toUInt();
+    res.type = getNextAtomicField(segment, start, false).toUInt();
     if (res.type == 0) { // TEXT
         res.value = STRING.match(segment).captured();
         // remove quotations
@@ -236,7 +236,7 @@ QList<EventValueSegment> Utils::parseTexts(QString segments) {
     //this is the actual array, loop over it
     QList<EventValueSegment> res;
     for (;;) {
-        QString value = getNextAtomicField(segments, start);
+        QString value = getNextAtomicField(segments, start, false);
         //if (value.size() == 0) break;
         qDebug() << "Segment: " << value;
         //qDebug() << start << " - " << segments.size();
@@ -318,12 +318,12 @@ EventValue Utils::parseEventValue(QString input)
     int start = 1;
     getNextAtomicField(input, start); //always null?
     getNextAtomicField(input, start); //always []?
-    QString content = getNextAtomicField(input, start);
+    QString content = getNextAtomicField(input, start, false);
     qDebug() << "CONT: " << content;
     start = 1;
-    QString segments = getNextAtomicField(content, start);
+    QString segments = getNextAtomicField(content, start, false);
     res.segments = parseTexts(segments);
-    QString attachments = getNextAtomicField(content, start);
+    QString attachments = getNextAtomicField(content, start, false);
     res.attachments = parseAttachments(attachments);
     qDebug() << "att done";
     return res;
@@ -366,7 +366,7 @@ Event Utils::parseEvent(QString conv)
     //skip 2 fields
     for (int i=0; i<2; i++)
         getNextAtomicField(conv, start);
-    QString message = getNextAtomicField(conv, start);
+    QString message = getNextAtomicField(conv, start, false);
     //This is empty in case of a call?
     if (message.size() > 1) {
         res.value = parseEventValue(message);

utils.h
@@ -34,9 +34,9 @@ public:
     Utils();
     static Identity parseIdentity(QString input);
     static int skipTextFields(QString input, int startPos);
-    static int skipFields(QString input, int startPos);
+    static int skipFields(QString input, int startPos, bool parseBracketsInString = true);
     static int skipFieldsForPush(QString input, int startPos);
-    static QString getNextAtomicField(QString conv, int &start);
+    static QString getNextAtomicField(QString conv, int &start, bool parseBracketsInString = true);
     static QString getNextAtomicFieldForPush(QString conv, int &start);
     static QString getNextField(QString conv, int start);
     static QString getTextAtomicField(QString conv, int &start);