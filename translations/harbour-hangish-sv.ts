b215babd98876cf51a8f5706a8a9215e3dabb69d
Åke Engelbrektson <eson57@users.noreply.github.com>
2015-07-18 16:39:55 +0200

    Create harbour-hangish-sv.ts
    Swedish translation for your client

---
+++ translations/harbour-hangish-sv.ts
@@ -0,0 +1,182 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="11"/>
+        <source>About Hangish</source>
+        <translation>Om Hangish</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="34"/>
+        <source>A client for Google Hangouts</source>
+        <translation>En klient för Google Hangouts</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="58"/>
+        <source>Hangish is open source software licensed under the terms of </source>
+        <translation>Hangish är öppen källkod och licensierad under villkoren för </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="59"/>
+        <source>the GNU General Public License.</source>
+        <translation>GNU General Public License.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="76"/>
+        <source>Donate</source>
+        <translation>Donera</translation>
+    </message>
+</context>
+<context>
+    <name>ConvRenameInput</name>
+    <message>
+        <location filename="../qml/pages/ConvRenameInput.qml" line="30"/>
+        <source>New name</source>
+        <translation>Nytt namn</translation>
+    </message>
+</context>
+<context>
+    <name>Conversation</name>
+    <message>
+        <location filename="../qml/pages/Conversation.qml" line="63"/>
+        <source>Load more...</source>
+        <translation>Läs in mer...</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Conversation.qml" line="86"/>
+        <location filename="../qml/pages/Conversation.qml" line="141"/>
+        <source>Reply</source>
+        <translation>Svara</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Conversation.qml" line="103"/>
+        <source>Can&apos;t send empty message</source>
+        <translation>Kan inte skicka tomt meddelande</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Conversation.qml" line="110"/>
+        <source>Sending message...</source>
+        <translation>Skickar meddelande...</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Conversation.qml" line="115"/>
+        <source>Sending image...</source>
+        <translation>Skickar bild...</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Conversation.qml" line="137"/>
+        <source> is typing</source>
+        <translation> skriver</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Conversation.qml" line="139"/>
+        <source> paused</source>
+        <translation> pausad</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Conversation.qml" line="173"/>
+        <source>Image saved to gallery!</source>
+        <translation>Bilden sparad i galleriet!</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="67"/>
+        <source> is typing</source>
+        <translation> skriver</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="49"/>
+        <source>Login Failed </source>
+        <translation>Inloggning misslyckades </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="77"/>
+        <source>Logging in</source>
+        <translation>Loggar in</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="95"/>
+        <source>Delete authentication cookie</source>
+        <translation>Radera autentiserings-cookie</translation>
+    </message>
+</context>
+<context>
+    <name>ImagePicker</name>
+    <message>
+        <location filename="../qml/pages/ImagePicker.qml" line="42"/>
+        <source>Select picture</source>
+        <translation>Välj bild</translation>
+    </message>
+</context>
+<context>
+    <name>Message</name>
+    <message>
+        <location filename="../qml/delegates/Message.qml" line="12"/>
+        <source>Download to gallery</source>
+        <translation>Ladda ner till galleriet</translation>
+    </message>
+</context>
+<context>
+    <name>Roster</name>
+    <message>
+        <location filename="../qml/pages/Roster.qml" line="55"/>
+        <source>About Hangish</source>
+        <translation>Om Hangish</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Roster.qml" line="59"/>
+        <source>Log out and exit</source>
+        <translation>Logga ut och avsluta</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Roster.qml" line="60"/>
+        <source>Deleting cookies</source>
+        <translation>Tar bort cookies</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Roster.qml" line="73"/>
+        <source>Conversations</source>
+        <translation>Konversationer</translation>
+    </message>
+</context>
+<context>
+    <name>RosterDelegate</name>
+    <message>
+        <location filename="../qml/delegates/RosterDelegate.qml" line="11"/>
+        <source>Rename</source>
+        <translation>Byt namn</translation>
+    </message>
+    <message>
+        <location filename="../qml/delegates/RosterDelegate.qml" line="19"/>
+        <source>Archive</source>
+        <translation>Arkiv</translation>
+    </message>
+    <message>
+        <location filename="../qml/delegates/RosterDelegate.qml" line="19"/>
+        <source>Leave group</source>
+        <translation>Lämna gruppen</translation>
+    </message>
+    <message>
+        <location filename="../qml/delegates/RosterDelegate.qml" line="21"/>
+        <source>Leaving conversation</source>
+        <translation>Lämnar konversationen</translation>
+    </message>
+    <message>
+        <location filename="../qml/delegates/RosterDelegate.qml" line="36"/>
+        <source>Reenable notifications</source>
+        <translation>Återaktivera aviseringar</translation>
+    </message>
+    <message>
+        <location filename="../qml/delegates/RosterDelegate.qml" line="36"/>
+        <source>Silence</source>
+        <translation>Tystnad</translation>
+    </message>
+</context>
+</TS>
